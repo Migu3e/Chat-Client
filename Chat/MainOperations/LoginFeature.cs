c27b1988a7b6f709f1435ea0b8cb1088a4548240
itay miguel <itaymigul@gmail.com>
2024-05-22 23:27:40 +0300

    Transfer to second pc 2

--- Chat/MainOperations/LoginFeature.cs
+++ Chat/MainOperations/LoginFeature.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Client.Firestore;
+using Google.Cloud.Firestore;
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +11,46 @@ namespace Chat.MainOperations
 {
     internal class LoginFeature
     {
+        
+        public static bool LoginNewUser(string username, string password)
+        {
+            if (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password))
+            {
+                Console.WriteLine("Error: No Fields");
+                return false;
+            }
+            else if (!MainOperations.CheckUsernameCondition(username) && !MainOperations.CheckPasswordCondition(password))
+            {
+                return false;
+            }
+            else
+            {
+                var db = FirestoreHelper.database;
+                DocumentReference docRef = db.Collection("UserData").Document(username);
+                DocumentSnapshot snapshot = docRef.GetSnapshotAsync().Result;
+
+                if (snapshot.Exists)
+                {
+                    UserData data = snapshot.ConvertTo<UserData>();
+                    if (password == data.Password)
+                    {
+                        Console.WriteLine("Login successful");
+                        return true;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Password doesn't match");
+                        return false;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Username doesn't exist");
+                    return false;
+                }
+            }
+
+        }
+
     }
 }
--- Chat/MainOperations/RegisterFeature.cs
+++ Chat/MainOperations/RegisterFeature.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Client.Firestore;
+using Google.Cloud.Firestore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,38 @@ namespace Chat.MainOperations
 {
     internal class RegisterFeature
     {
+        public static bool RegisterNewUser(string currUsername, string currPassword, string currEmail)
+        {
+            if (string.IsNullOrEmpty(currUsername) || string.IsNullOrEmpty(currPassword) || string.IsNullOrEmpty(currEmail))
+            {
+                Console.WriteLine("Fields are empty");
+                return false;
+            }
+
+            var db = FirestoreHelper.database;
+            if (MainOperations.CheckIfAlreadyExist(currUsername, currEmail))
+            {
+                // Display message handled in CheckIfAlreadyExist
+                return false;
+            }
+
+            if (!MainOperations.CheckEmailCondition(currEmail) & !MainOperations.CheckUsernameCondition(currUsername) && !MainOperations.CheckPasswordCondition(currPassword))
+            {
+                return false;
+            }
+
+            var data = new UserData
+            {
+                UserName = currUsername,
+                Password = currPassword,
+                Email = currEmail
+            };
+
+            DocumentReference docRef = db.Collection("UserData").Document(data.UserName);
+            docRef.SetAsync(data).Wait(); // Using Wait() to make it synchronous
+            Console.WriteLine("User registered successfully");
+            return true;
+        }
+
     }
 }
